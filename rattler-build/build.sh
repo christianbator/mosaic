be4b3230373ab3ce2a5e633635a713d4895e1e8d
Christian Bator <christian@bator.co>
2025-03-13 12:43:01 +0100

    Put swift libvisualizer behind OSX_ARCH in rattler-build build.sh

--- rattler-build/build.sh
+++ rattler-build/build.sh
@@ -5,18 +5,31 @@
 # Created by Christian Bator on 01/02/2025
 #
 
+set -euo pipefail
+
+#
+# Locations
+#
 mkdir -p $PREFIX/lib/mojo
 mkdir -p $PREFIX/lib/mosaic
 
+#
 # Build libcodec
+#
 cp external/stb/{stb_image.h,stb_image_write.h} libcodec
-
 clang -fPIC -shared -Wall -Werror -DSTBI_NEON -o $PREFIX/lib/mosaic/libcodec.dylib libcodec/libcodec.c
 
+#
 # Build libvisualizer
-swift_source_files=$(find libvisualizer/mac/MacVisualizer -name "*.swift")
+#
+if [ -n "$OSX_ARCH" ]; then
+    swift_source_files=$(find libvisualizer/mac/MacVisualizer -name "*.swift")
+    swiftc -emit-library -o $PREFIX/lib/mosaic/libvisualizer.dylib $swift_source_files
+else
+    echo "> Unsupported platform for libvisualizer"
+fi
 
-swiftc -emit-library -o $PREFIX/lib/mosaic/libvisualizer.dylib $swift_source_files
-
+#
 # Build mosaic
+#
 mojo package mosaic -o $PREFIX/lib/mojo/mosaic.mojopkg
